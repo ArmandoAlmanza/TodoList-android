17acbf588447ba8c94f136dc31f26f47d6356c9c
Armando Almanza <armando.s.almanza@gmail.com>
2022-10-13 16:21:52 -0500

    change vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    server: {
+        port: 8080,
+        open: true,
+    },
+    preview: {
+        port: 8000,
+    },
+    build: {
+        outDir: "./dist",
+    },
+});
